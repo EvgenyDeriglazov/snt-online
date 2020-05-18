b1b170795e0dfd8b7fe39d69d0c3e80d5e98dddf
Evgeny Deriglazov <evgenyderiglazov@yandex.ru>
2020-05-18 13:23:16 +0300

    Finalize snt model and test file for snt model in index.tests.test_model_snt.py

--- index/tests/test_model_snt.py
+++ index/tests/test_model_snt.py
@@ -21,7 +21,7 @@ class SntModelTest(TestCase):
             first_name='Иван',
             middle_name='Иванович',
             last_name='Иванов',
-        )
+            )
         Snt.objects.create(
             name='СНТ Бобровка',
             personal_acc='01234567898765432101',
@@ -31,88 +31,102 @@ class SntModelTest(TestCase):
             inn='0123456789',
             kpp='123456789',
             chair_man=ChairMan.objects.get(id=1),
-        )
+            address="123СТАР",
+            )
     # Test functions
     def test_name_field(self):
-        snt = Snt.objects.get(id=1)
-        field_label = snt._meta.get_field('name').verbose_name
-        help_text = snt._meta.get_field('name').help_text
-        max_length = snt._meta.get_field('name').max_length
+        obj = Snt.objects.get(id=1)
+        field_label = obj._meta.get_field('name').verbose_name
+        help_text = obj._meta.get_field('name').help_text
+        max_length = obj._meta.get_field('name').max_length
         self.assertEquals(field_label, 'Название СНТ')   
         self.assertEqual(max_length, 200)
         self.assertEqual(help_text, 'Полное название СНТ')
 
     def test_personal_acc_field(self):
-        snt = Snt.objects.get(id=1)
-        field_label = snt._meta.get_field('personal_acc').verbose_name
-        max_length = snt._meta.get_field('personal_acc').max_length
-        help_text = snt._meta.get_field('personal_acc').help_text
+        obj = Snt.objects.get(id=1)
+        field_label = obj._meta.get_field('personal_acc').verbose_name
+        max_length = obj._meta.get_field('personal_acc').max_length
+        help_text = obj._meta.get_field('personal_acc').help_text
         self.assertEqual(field_label, 'Номер расчетного счета')
         self.assertEqual(max_length, 20)
         self.assertEqual(help_text, 'Номер расчетного счета (20-и значное число)')
 
     def test_bank_name_field(self):
-        snt = Snt.objects.get(id=1)
-        field_label = snt._meta.get_field('bank_name').verbose_name
-        max_length = snt._meta.get_field('bank_name').max_length
-        help_text = snt._meta.get_field('bank_name').help_text
+        obj = Snt.objects.get(id=1)
+        field_label = obj._meta.get_field('bank_name').verbose_name
+        max_length = obj._meta.get_field('bank_name').max_length
+        help_text = obj._meta.get_field('bank_name').help_text
         self.assertEqual(field_label, 'Наименование банка получателя')
         self.assertEqual(max_length, 45)
         self.assertEqual(help_text, 'Наименование банка получателя')
 
     def test_bic_field(self):
-        snt = Snt.objects.get(id=1)
-        field_label = snt._meta.get_field('bic').verbose_name
-        max_length = snt._meta.get_field('bic').max_length
-        help_text = snt._meta.get_field('bic').help_text
+        obj = Snt.objects.get(id=1)
+        field_label = obj._meta.get_field('bic').verbose_name
+        max_length = obj._meta.get_field('bic').max_length
+        help_text = obj._meta.get_field('bic').help_text
         self.assertEqual(field_label, 'БИК')
         self.assertEqual(max_length, 9)
         self.assertEqual(help_text, 'БИК (9-и значное число)')
 
     def test_corresp_acc_field(self):
-        snt = Snt.objects.get(id=1)
-        field_label = snt._meta.get_field('corresp_acc').verbose_name
-        max_length = snt._meta.get_field('corresp_acc').max_length
-        help_text = snt._meta.get_field('corresp_acc').help_text
+        obj = Snt.objects.get(id=1)
+        field_label = obj._meta.get_field('corresp_acc').verbose_name
+        max_length = obj._meta.get_field('corresp_acc').max_length
+        help_text = obj._meta.get_field('corresp_acc').help_text
         self.assertEqual(field_label, 'Номер кор./счета')
         self.assertEqual(max_length, 20)
         self.assertEqual(help_text, 'Номер кор./счета (20-и значное число)')
 
     def test_inn_field(self):
-        snt = Snt.objects.get(id=1)
-        field_label = snt._meta.get_field('inn').verbose_name
-        max_length = snt._meta.get_field('inn').max_length
-        help_text = snt._meta.get_field('inn').help_text
+        obj = Snt.objects.get(id=1)
+        field_label = obj._meta.get_field('inn').verbose_name
+        max_length = obj._meta.get_field('inn').max_length
+        help_text = obj._meta.get_field('inn').help_text
         self.assertEqual(field_label, 'ИНН')
         self.assertEqual(max_length, 10)
         self.assertEqual(help_text, 'ИНН (10-и значное число)')
 
     def test_kpp_field(self):
-        snt = Snt.objects.get(id=1)
-        field_label = snt._meta.get_field('kpp').verbose_name
-        max_length = snt._meta.get_field('kpp').max_length
-        help_text = snt._meta.get_field('kpp').help_text
+        obj = Snt.objects.get(id=1)
+        field_label = obj._meta.get_field('kpp').verbose_name
+        max_length = obj._meta.get_field('kpp').max_length
+        help_text = obj._meta.get_field('kpp').help_text
         self.assertEqual(field_label, 'КПП')
         self.assertEqual(max_length, 9)
         self.assertEqual(help_text, 'КПП (9-и значное число)')
 
+    def test_address_field(self):
+        obj = Snt.objects.get(id=1)
+        field_label = obj._meta.get_field('address').verbose_name
+        max_length = obj._meta.get_field('address').max_length
+        help_text = obj._meta.get_field('address').help_text
+        self.assertEqual(field_label, 'Адрес')
+        self.assertEqual(max_length, 200)
+        self.assertEqual(
+            help_text, 
+            'Полный адрес садоводства включая область и р-он',
+            )
+        self.assertEqual(obj.address, '123СТАР')
+ 
     def test_chair_man_field(self):
-        snt = Snt.objects.get(id=1)
+        obj = Snt.objects.get(id=1)
         ch_m_obj = ChairMan.objects.get(id=1)
-        field_label = snt._meta.get_field('chair_man').verbose_name
-        help_text = snt._meta.get_field('chair_man').help_text
-        self.assertEqual(snt.chair_man, ch_m_obj)
+        field_label = obj._meta.get_field('chair_man').verbose_name
+        help_text = obj._meta.get_field('chair_man').help_text
+        self.assertEqual(obj.chair_man, ch_m_obj)
         self.assertEqual(field_label, 'председатель')
         self.assertEqual(help_text, 'председатель садоводства')
     
     def test_object_name(self):
-        snt = Snt.objects.get(id=1)
-        object_name = f'{snt.name}'
-        self.assertEquals(object_name, str(snt))
+        obj = Snt.objects.get(id=1)
+        object_name = f'{obj.name}'
+        self.assertEquals(object_name, str(obj))
 
-#    def test_get_absolute_url(self):
-#        snt = Snt.objects.get(id=1)
-#        self.assertEquals(snt.get_absolute_url(), '/data/snt-detail/1')
+    def test_get_absolute_url(self):
+        obj = Snt.objects.get(id=1)
+        self.assertEquals(obj.get_absolute_url(), '/data/obj-detail/1')
 
     def test_verbose_names(self):
         self.assertEquals(Snt._meta.verbose_name, 'СНТ')
