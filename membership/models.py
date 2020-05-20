f792008db17bdf485277ddb33acb52380729c059
Evgeny Deriglazov <evgenyderiglazov@yandex.ru>
2020-05-20 14:37:56 +0300

    Draft MPayment model and test code in membership.models and membership.tests.test_model_m_payment

--- membership/models.py
+++ membership/models.py
@@ -1,3 +1,69 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from index.models import LandPlot
 
 # Create your models here.
+class MPayment(models.Model):
+    """Represents membership payment."""
+    payment_date = models.DateTimeField(
+        "Дата оплаты",
+        help_text="Фактическая дата оплаты",
+        blank=True,
+        )
+    year_period = models.CharField(
+        "Год",
+        max_length=4,
+        help_text="Членский взнос за определенный год",
+        )
+
+    month_period = models.CharField(
+        "Месяц",
+        help_text="Членский взнос за определенный месяц",
+        )
+    fee = models.DecimalField(
+        "Размер взноса",
+        help_text="Размер членского взноса за сотку (100 м.кв)",
+        max_digits=7,
+        decimal_places=2,
+        )
+    plot_area = models.PositiveIntegerField(
+    	"Площадь участка",
+    	help_text="Площадь участка в метрах квадратных",
+    	)
+    fee_amount = models.DecimalField(
+    	"Сумма",
+        help_text="Общая сумма к оплате",
+        max_digits=7,
+        decimal_places=2,
+    	)
+    land_plot = models.ForeignKey(
+        LandPlot,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name="Номер участка",
+        help_text="Номер участка",
+    	)
+   	STATUS = [
+        ('n', 'Новые показания'),
+        ('p', 'Оплачено'),
+        ('c', 'Оплата подтверждена'),
+    	] 
+    status = models.CharField(
+        "Статус",
+        max_length=1,
+        choices=STATUS,
+        default='n',
+        help_text="Статус квитанции",
+    ) 
+    class Meta:
+        verbose_name = "членский взнос"
+        verbose_name_plural = "членские взносы"
+
+    def __str__(self):
+        """String to represent the Model(class) object."""
+        return self.year_period + " " + self.land_plot.plot_number
+
+    def get_absolute_url(self):
+        """Returns url to access an instance of the model."""
+        pass
---
+++ membership/tests/test_model_m_payment.py
@@ -0,0 +1,95 @@
+# --- Collectstatic (ValueError: Missing staticfiles manifest entry...) ---
+# python3 manage.py collectstatic
+# --- Verbosity ---
+# python3 manage.py test --verbosity (1-default, 0, 1, 2, 3)
+# --- Run specific test modules ---
+# python3 manage.py test data.tests
+# python3 manage.py test data.tests.test_models
+# python3 manage.py test data.tests.test_models.TestClass
+# --- Coverage.py --- 
+# coverage run --source='.' manage.py test <appname>
+# coverage report
+
+from django.test import TestCase
+from index.models import *
+from membership.models import *
+from django.contrib.auth.models import User
+
+class MPaymentModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        MPayment.objects.create(
+            first_name='Иван',
+            middle_name='Иванович',
+            last_name='Иванов',
+            )
+        Snt.objects.create(
+            name='СНТ Бобровка',
+            personal_acc='01234567898765432101',
+            bank_name='Банк',
+            bic='123456789',
+            corresp_acc='01234567898765432101',
+            inn='0123456789',
+            kpp='123456789',
+            chair_man=ChairMan.objects.get(id=1),
+            )
+        Owner.objects.create(
+            first_name="Сергей",
+            middle_name="Сергеевич",
+            last_name="Сергеев",
+        )
+        LandPlot.objects.create(
+            plot_number="10",
+            plot_area=6000,
+            snt=Snt.objects.get(id=1),
+            owner=Owner.objects.get(id=1),
+        )
+    # Test functions
+    def test_plot_number_field(self):
+        obj = LandPlot.objects.get(id=1)
+        field = obj._meta.get_field('plot_number')
+        self.assertEqual(field.verbose_name, "Номер участка")
+        self.assertEqual(field.max_length, 10)
+        self.assertEqual(field.help_text, "Номер участка")
+        self.assertEqual(field.unique, True)
+        self.assertEqual(obj.plot_number, "10")
+
+    def test_plot_area_field(self):
+        obj = LandPlot.objects.get(id=1)
+        field = obj._meta.get_field('plot_area')
+        self.assertEqual(field.verbose_name, "Размер участка")
+        self.assertEqual(field.help_text, "Единица измерения кв.м")
+        self.assertEqual(obj.plot_area, 6000)
+
+    def test_snt_field(self):
+        obj = LandPlot.objects.get(id=1)
+        snt_obj = Snt.objects.get(id=1)
+        field = obj._meta.get_field('snt')
+        self.assertEqual(field.verbose_name, "СНТ")
+        self.assertEqual(field.help_text, "Расположен в СНТ")
+        self.assertEqual(obj.snt, snt_obj)
+
+    def test_owner_field(self):
+        obj = LandPlot.objects.get(id=1)
+        owner_obj = Owner.objects.get(id=1)
+        field = obj._meta.get_field('owner')
+        #on_delete = obj._meta.get_field('owner').on_delete
+        self.assertEqual(field.verbose_name, "Владелец участка")
+        self.assertEqual(field.help_text, "Владелец участка")
+        self.assertEqual(field.null, True)
+        #self.assertEqual(on_delete, models.SET_NULL)
+        self.assertEqual(obj.owner, owner_obj)
+
+    def test_object_name(self):
+        obj = LandPlot.objects.get(id=1)
+        object_name = f"{obj.plot_number}"
+        self.assertEquals(object_name, obj.__str__())
+        # or self.assertEquals(object_name, str(obj))
+    
+    def test_get_absolute_url(self):
+        obj = LandPlot.objects.get(id=1)
+        self.assertEquals(obj.get_absolute_url(), "/data/land-plot-detail/1")
+
+    def test_verbose_names(self):
+        self.assertEquals(LandPlot._meta.verbose_name, "участок")
+        self.assertEquals(LandPlot._meta.verbose_name_plural, "участки")
