b805c1d5ac7f42c82d684c6ff7fc859f85e597f0
Evgeny Deriglazov <evgenyderiglazov@yandex.ru>
2020-05-21 19:39:24 +0300

    Create TPayment model and test code. Refactor and fix some bugs in membership app test code for other models.

--- membership/models.py
+++ membership/models.py
@@ -160,3 +160,57 @@ class MRate(models.Model):
     def get_absolute_url(self):
         """Returns url to access an instance of the model."""
         pass
+
+class TPayment(models.Model):
+    """Represents target payment with detailed information about target,
+    amount, status and land plot."""
+    payment_date = models.DateField(
+        "Дата оплаты",
+        help_text="Фактическая дата оплаты",
+        blank=True,
+        null=True,
+        )
+    target = models.CharField(
+        "Цель",
+        max_length=200,
+        help_text="Укажите назначение целевого взноса",
+        )
+    amount = models.DecimalField(
+        "Сумма",
+        help_text="Сумма взноса к оплате",
+        max_digits=7,
+        decimal_places=2,
+        )
+    land_plot = models.ForeignKey(
+        LandPlot,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name="Участок",
+        help_text="Выберите участок",
+        )
+    STATUS_CHOICES = [
+        ('n', 'Неоплачено'),
+        ('p', 'Оплачено'),
+        ('c', 'Оплата подтверждена'),
+        ] 
+    status = models.CharField(
+        "Статус",
+        max_length=1,
+        choices=STATUS_CHOICES,
+        default='n',
+        help_text="Статус оплаты",
+        ) 
+ 
+    class Meta:
+        verbose_name = "целевой взнос"
+        verbose_name_plural = "целевые взносы"
+
+    def __str__(self):
+        """String to represent the Model(class) object."""
+        return self.target + " " + self.land_plot.plot_number
+
+    def get_absolute_url(self):
+        """Returns url to access an instance of the model."""
+        pass
+
+
--- membership/tests/test_model_t_payment.py
+++ membership/tests/test_model_t_payment.py
@@ -17,7 +17,7 @@ from django.contrib.auth.models import User
 from membership.validators import *
 import datetime
 
-class MPaymentModelTest(TestCase):
+class TPaymentModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         ChairMan.objects.create(
@@ -48,17 +48,15 @@ class MPaymentModelTest(TestCase):
             snt=Snt.objects.get(id=1),
             owner=Owner.objects.get(id=1),
             )
-        MPayment.objects.create(
-            year_period='2020',
-            rate=1000,
-            plot_area=LandPlot.objects.get(id=1).plot_area,
+        TPayment.objects.create(
+            target="Test target",
             amount=6000,
             land_plot=LandPlot.objects.get(id=1),
             status='n',
             )
     # Test functions
     def test_payment_date_field(self):
-        obj = MPayment.objects.get(id=1)
+        obj = TPayment.objects.get(id=1)
         field = obj._meta.get_field('payment_date')
         self.assertEqual(field.verbose_name, "Дата оплаты")
         self.assertEqual(field.help_text, "Фактическая дата оплаты")
@@ -66,67 +64,16 @@ class MPaymentModelTest(TestCase):
         self.assertEqual(field.null, True)
         self.assertEqual(obj.payment_date, None)
 
-    def test_year_period_field(self):
-        obj = MPayment.objects.get(id=1)
-        field = obj._meta.get_field('year_period')
-        self.assertEqual(field.verbose_name, "Год")
-        self.assertEqual(field.max_length, 4)
-        self.assertEqual(field.help_text, "Укажите год в виде 4-х значного числа")
-        self.assertEqual(
-            field.validators[0:2],
-            [validate_number, validate_year_period_min_length]
-            )
-        self.assertEqual(obj.year_period, '2020')
-
-    def test_month_period_field(self):
-        obj = MPayment.objects.get(id=1)
-        field = obj._meta.get_field('month_period')
-        MONTH_PERIOD_CHOICES = [
-        ('', ''),
-        ('Jan', 'Январь'),
-        ('Feb', 'Февраль'),
-        ('Mar', 'Март'),
-        ('Apr', 'Апрель'),
-        ('May', 'Май'),
-        ('Jun', 'Июнь'),
-        ('Jul', 'Июль'),
-        ('Aug', 'Август'),
-        ('Sep', 'Сентябрь'),
-        ('Oct', 'Октябрь'),
-        ('Nov', 'Ноябрь'),
-        ('Dec', 'Декабрь'),
-        ]
-        self.assertEqual(field.verbose_name, "Месяц")
-        self.assertEqual(field.max_length, 3)
-        self.assertEqual(
-            field.help_text,
-            "Выберите месяц, если начисления"
-            + " членских взносов расчитываются помесячно"
-            )
-        self.assertEqual(field.blank, True)
-        self.assertEqual(field.choices, MONTH_PERIOD_CHOICES)
-        self.assertEqual(field.default, '')
-        self.assertEqual(obj.month_period, '')
-
-    def test_rate_field(self):
-        obj = MPayment.objects.get(id=1)
-        field = obj._meta.get_field('rate')
-        self.assertEqual(field.verbose_name, "Размер взноса")
-        self.assertEqual(
-            field.help_text,
-            "Размер членского взноса за сотку (100 м.кв)/рублей"
-            )
-        self.assertEqual(obj.plot_area, 6000)
-
-    def test_plot_area_field(self):
-        obj = MPayment.objects.get(id=1)
-        field = obj._meta.get_field('plot_area')
-        self.assertEqual(field.verbose_name, "Площадь участка")
-        self.assertEqual(field.help_text, "Площадь участка в квадратных метрах")
-        self.assertEqual(obj.plot_area, 6000)
+    def test_target_field(self):
+        obj = TPayment.objects.get(id=1)
+        field = obj._meta.get_field('target')
+        self.assertEqual(field.verbose_name, "Цель")
+        self.assertEqual(field.max_length, 200)
+        self.assertEqual(field.help_text, "Укажите назначение целевого взноса")
+        self.assertEqual(obj.target, "Test target")
 
     def test_amount_field(self):
-        obj = MPayment.objects.get(id=1)
+        obj = TPayment.objects.get(id=1)
         field = obj._meta.get_field('amount')
         self.assertEqual(field.verbose_name, "Сумма")
         self.assertEqual(field.help_text, "Сумма взноса к оплате")
@@ -135,7 +82,7 @@ class MPaymentModelTest(TestCase):
         self.assertEqual(obj.amount, 6000)
 
     def test_land_plot_field(self):
-        obj = MPayment.objects.get(id=1)
+        obj = TPayment.objects.get(id=1)
         plot_obj = LandPlot.objects.get(id=1)
         field = obj._meta.get_field('land_plot')
         #on_delete = obj._meta.get_field('owner').on_delete
@@ -146,7 +93,7 @@ class MPaymentModelTest(TestCase):
         self.assertEqual(obj.land_plot, plot_obj)
 
     def test_status_field(self):
-        obj = MPayment.objects.get(id=1)
+        obj = TPayment.objects.get(id=1)
         field = obj._meta.get_field('status')
         STATUS_CHOICES = [
             ('n', 'Неоплачено'),
@@ -163,15 +110,15 @@ class MPaymentModelTest(TestCase):
         self.assertEqual(obj.status, 'n')
 
     def test_meta_options(self):
-        self.assertEquals(MPayment._meta.verbose_name, "членский взнос")
-        self.assertEquals(MPayment._meta.verbose_name_plural, "членские взносы")
+        self.assertEquals(TPayment._meta.verbose_name, "целевой взнос")
+        self.assertEquals(TPayment._meta.verbose_name_plural, "целевые взносы")
    
     def test_str_method(self):
-        obj = MPayment.objects.get(id=1)
-        object_name = f"{obj.year_period} {obj.land_plot.plot_number}"
+        obj = TPayment.objects.get(id=1)
+        object_name = f"{obj.target} {obj.land_plot.plot_number}"
         self.assertEquals(object_name, obj.__str__())
         # or self.assertEquals(object_name, str(obj))
  
     def test_get_absolute_url(self):
-        obj = MPayment.objects.get(id=1)
+        obj = TPayment.objects.get(id=1)
         self.assertEquals(obj.get_absolute_url(), "/data/land-plot-detail/1")
