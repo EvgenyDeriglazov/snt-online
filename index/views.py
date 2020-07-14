ec4d4723223569d4dfee8f2e829d81f5d3e0d758
Evgeny Deriglazov <evgenyderiglazov@yandex.ru>
2020-07-14 19:22:02 +0300

    Create draft view function homepage() in Index app and setup root URLConf.

--- index/views.py
+++ index/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def homepage(request):
+    """View function for home page of site."""
+    pass
--- snt_online/urls.py
+++ snt_online/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from index.views import homepage
 
 urlpatterns = [
+    path('', homepage, name='homepage'),
     path('admin/', admin.site.urls),
 ]
 
