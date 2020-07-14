962309bf2111c1cea67dec4730cdb9ce09e66b74
Evgeny Deriglazov <evgenyderiglazov@yandex.ru>
2020-07-14 21:36:59 +0300

    Recommit untracked changes in ../index/static/ and ../index/templates/.

--- index/views.py
+++ index/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
     """View function for home page of site."""
-    pass
+    context = {
+        'x': 5,
+    }
+
+    return render(request, 'homepage.html', context=context)
