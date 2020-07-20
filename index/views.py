f7d6914ace7f2d312fd109f8521172b7be3701a4
Evgeny Deriglazov <evgenyderiglazov@yandex.ru>
2020-07-20 20:48:44 +0300

    Create block title, block top-bar and block left-column. Update base_generic.html and homepage.html

--- index/views.py
+++ index/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+from index.models import *
 
 # Create your views here.
 def homepage(request):
     """View function for home page of site."""
+    snt = Snt.objects.all()
+    if len(snt) > 0:
+        snt_name = snt[0].name 
+    else:
+        snt_name = "SNT Online"
+    
+    
     context = {
-        'x': 5,
+        'snt_name': snt_name,
     }
 
     return render(request, 'homepage.html', context=context)
