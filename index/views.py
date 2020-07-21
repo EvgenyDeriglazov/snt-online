c71e40c978f14e8ca6155596aa2c20701009ae4f
Evgeny Deriglazov <evgenyderiglazov@yandex.ru>
2020-07-21 08:53:03 +0300

    Update index.views add snt_address to bottom of homepage.html template, update base_generic.html template, modify index_styles.css.

--- index/views.py
+++ index/views.py
@@ -6,13 +6,16 @@ def homepage(request):
     """View function for home page of site."""
     snt = Snt.objects.all()
     if len(snt) > 0:
-        snt_name = snt[0].name 
+        snt_name = snt[0].name
+        snt_address = snt[0].address
     else:
-        snt_name = "SNT Online"
+        snt_name = "СНТ Онлайн"
+        snt_address = "Адрес СНТ Онлайн"
     
     
     context = {
         'snt_name': snt_name,
+        'snt_address': snt_address,
     }
 
     return render(request, 'homepage.html', context=context)
