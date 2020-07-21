9817555861eb6a4beae1b712159cc9e46a059c5c
Evgeny Deriglazov <evgenyderiglazov@yandex.ru>
2020-07-21 12:13:05 +0300

    Add block menu and update base_generic.html, homepage.html templates. Add url link to home page in menu.

--- index/views.py
+++ index/views.py
@@ -4,18 +4,22 @@ from index.models import *
 # Create your views here.
 def homepage(request):
     """View function for home page of site."""
+    context = {}
     snt = Snt.objects.all()
+    info_list = Info.objects.filter(
+        status__exact='published',
+        ).order_by('-pub_date')
+    #land_plot_list = snt[0].entry_set.all()
     if len(snt) > 0:
-        snt_name = snt[0].name
-        snt_address = snt[0].address
+        context['snt_name'] = snt[0].name
+        context['snt_address'] = snt[0].address
     else:
-        snt_name = "СНТ Онлайн"
-        snt_address = "Адрес СНТ Онлайн"
+        context['snt_name'] = "СНТ Онлайн"
+        context['snt_address'] = "Адрес СНТ Онлайн"
     
-    
-    context = {
-        'snt_name': snt_name,
-        'snt_address': snt_address,
-    }
+    #if len(info_list) > 0:
+    #    context['info_list'] = info_list
+
+    #context['land_plots_count'] = len(land_plot_list)
 
     return render(request, 'homepage.html', context=context)
