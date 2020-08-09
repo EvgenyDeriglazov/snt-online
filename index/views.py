b889e74c41629203c051730a4c69830f24997cbf
Evgeny Deriglazov <evgenyderiglazov@yandex.ru>
2020-08-09 10:38:58 +0300

    Change some class based views from ListView to TemplateView in Index app.

--- index/views.py
+++ index/views.py
@@ -1,30 +1,29 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 from index.models import *
 from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
-class HomePage(ListView):
+class HomePage(TemplateView):
     """Class based view to display homepage."""
-    model = Snt
     template_name = "home_page.html"
-    context_object_name = "snt_list"
+#    context_object_name = "snt_list"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['snt_list'] = Snt.objects.all()
         context['land_plots_count'] = LandPlot.objects.count()
         context['auth_form'] = AuthenticationForm
         context['user_name'] = str(get_model_by_user(self.request.user))
         return context
 
-class InfoPage(ListView):
+class InfoPage(TemplateView):
     """Class based view to display Info model page."""
-    model = Snt
     template_name = "info_page.html"
-    context_object_name = "snt_list"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['snt_list'] = Snt.objects.all()
         context['info_list'] = Info.objects.filter(
             status__exact='published',
             ).order_by('-pub_date')
@@ -46,40 +45,37 @@ class InfoDetailsPage(DetailView):
         return context
 
 
-class SntBankDetailsPage(ListView):
+class SntBankDetailsPage(TemplateView):
     """Class based view to display Snt bank details page."""
-    model = Snt
     template_name = "snt_bank_details_page.html"
-    context_object_name = "snt_list"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['snt_list'] = Snt.objects.all()
         context['auth_form'] = AuthenticationForm
         context['user_name'] = str(get_model_by_user(self.request.user))
         return context
 
-class SntContactsPage(ListView):
+class SntContactsPage(TemplateView):
     """Class based view to display Snt contacts."""
-    model = Snt
     template_name = "snt_contacts_page.html"
-    context_object_name = "snt_list"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['snt_list'] = Snt.objects.all()
         context['auth_form'] = AuthenticationForm
         context['user_name'] = str(get_model_by_user(self.request.user))
         context['chair_man'] = context['snt_list'][0].chair_man
         context['accountant'] = context['snt_list'][0].accountant
         return context
 
-class DocsPage(ListView):
+class DocsPage(TemplateView):
     """Class based view to display Docs."""
-    model = Snt
     template_name = "docs_page.html"
-    context_object_name = "snt_list"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['snt_list'] = Snt.objects.all()
         if Docs.objects.filter(status__exact='published').exists():
             context['docs_list'] = Docs.objects.filter(
                 status__exact='published').order_by('-upload_date')
@@ -103,7 +99,7 @@ class DocsDetailsPage(DetailView):
 
 # Re-use helper functions
 def get_model_by_user(user):
-    """Returns related model in a one-to-one relationship to user.
+    """Returns related model via one-to-one relationship to user.
     If model does not exist returns user.username.
     If user is not authenticated returns None."""
     if user.is_authenticated:
