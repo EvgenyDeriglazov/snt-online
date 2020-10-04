399f8830b58afa72821857dbba36b6c38a55cafd
Evgeny Deriglazov <evgenyderiglazov@yandex.ru>
2020-10-04 16:56:07 +0300

    Final update to css for light-demo

--- index/urls.py
+++ index/urls.py
@@ -5,10 +5,10 @@ from index.views import *
 
 urlpatterns = [
 	path('', HomePage.as_view(), name='home'),
-	path('info/', InfoPage.as_view(), name='info'),
-	path('info/<int:pk>/', InfoDetailsPage.as_view(), name='info-details'),
-	path('docs/', DocsPage.as_view(), name='docs'),
-    path('docs/<int:pk>/', DocsDetailsPage.as_view(), name='docs-details'),
+	#path('info/', InfoPage.as_view(), name='info'),
+	#path('info/<int:pk>/', InfoDetailsPage.as_view(), name='info-details'),
+	#path('docs/', DocsPage.as_view(), name='docs'),
+    #path('docs/<int:pk>/', DocsDetailsPage.as_view(), name='docs-details'),
     path(
         'snt-bank-details/',
         SntBankDetailsPage.as_view(),
