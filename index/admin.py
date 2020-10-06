4fde8d6b8bba84c2d9475edc3c1a943dfa18fade
Evgeny Deriglazov <evgenyderiglazov@yandex.ru>
2020-10-06 16:07:02 +0300

    Update admin Owner model. Draft for admin LandPlot model.

--- index/admin.py
+++ index/admin.py
@@ -3,9 +3,23 @@ from .models import *
 
 # Register your models here.
 admin.site.register(Snt)
-admin.site.register(LandPlot)
+#admin.site.register(LandPlot)
 admin.site.register(ChairMan)
-admin.site.register(Owner)
+#admin.site.register(Owner)
 admin.site.register(Accountant)
 admin.site.register(Docs)
 admin.site.register(Info)
+
+@admin.register(Owner)
+class OwnerAdmin(admin.ModelAdmin):
+	list_display = (
+		'__str__', 'user', 'join_date', 'leave_date',
+		)
+	list_filter = ('leave_date',)
+	
+@admin.register(LandPlot)
+class LandPlotAdmin(admin.ModelAdmin):
+	list_display = (
+		'plot_number', 'plot_area', 'owner',
+		)
+	admin_order_field = '-plot_number'
